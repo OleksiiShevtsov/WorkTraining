4e691b10f25df5a89ec2d11e435a9199a1c0b468
OleksiiShevtsov <70405962+OleksiiShevtsov@users.noreply.github.com>
2021-11-15 14:38:26 +0200

    Update main.cpp

--- Trainee/Stroustrup/task_1/main.cpp
+++ Trainee/Stroustrup/task_1/main.cpp
@@ -1,16 +1,18 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
 
 int main()
 {
-	std::string prev = "w";
-
-	std::string curr;
-	while (std::cin >> curr) {
-		if (prev == curr) {
-			std::cout << "repit word: " << curr << std::endl;
-			prev = curr;
-			break;
+	int number_of_words = 0;
+	std::string previous = "w"; // Не слово 
+	std::string current;
+	while (std::cin >> current) {
+		++number_of_words; // Увеличива ем счетчик слов 
+		if (previous == current) {
+			std::cout << "repetition of a word: " << current
+				<< " after " << number_of_words
+				<< " words." << std::endl;
+			previous = current;
 		}
 	}
 }
