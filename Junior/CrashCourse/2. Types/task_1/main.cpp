5b85b6ac81e80b8286dc1961e06cfbe3f35bf2ea
OleksiiShevtsov <70405962+OleksiiShevtsov@users.noreply.github.com>
2021-12-21 01:46:29 +0200

    Jun_CC_t_2

--- Junior/CrashCourse/2. Types/task_1/main.cpp
+++ Junior/CrashCourse/2. Types/task_1/main.cpp
@@ -1,15 +1,28 @@
 #include <cstdio>
+#include <cstddef>
+#include <iostream>
 
 int main()
 {
     unsigned short a = 0b10101010;
     printf("%hu\n", a);
 
-    int b = 0123;
-    printf("%d\n", b);
-
-    unsigned long long d = 0xFFFFFFFFFFFFFFFF;
-    printf("%llu\n", d);
+    double an = 6.0221409e23;
+    printf("Avogardo's Number: %le %lf %lg \n", an, an, an);
     
+    char x = 'M';
+    wchar_t y = L'Z';
+    printf("Windows binaries start with %c%lc.\n", x, y);
+
+    bool b1 = true, b2 = false;
+    printf("%d %d\n", b1, b2);
+
+    printf("1 == 1: %d\n", 1 == 1);
+
+    std::size_t size = sizeof(int);
+    printf("size: %zu\n", size);
+
+    int my_array[2] = { 0, 1 };
+
     return 0;
 }
