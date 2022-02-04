8843b2703d2797aff2a29cc0dcf958308350b34b
alexey.shevtsov <alexey.shevtsov@apriorit.com>
2022-02-04 15:09:10 +0200

    Jun_CC_t_3

--- Junior/CrashCourse/3. Reference types/task_1/main.cpp
+++ Junior/CrashCourse/3. Reference types/task_1/main.cpp
@@ -3,9 +3,31 @@
 #include <iostream>
 #include <cwchar> // for wprintf
 
+struct JBall{
+    int getX(){return m_x;}
+    int getY(){return m_y;}
+    void setX(int x){m_x = x;}
+    void setY(int y){m_y = y;}
+private:
+    int m_x;
+    int m_y;
+};
+
 int main()
 {
-    int
+    uint32_t i{};
+    uint32_t *i_ptr{ &i };
+    printf("i: %d, i_ptr: %p;\n", i, i_ptr);
+    *i_ptr = 100;
+    printf("*i_ptr: %d, i_ptr: %p;\n", *i_ptr, i_ptr);
+
+    JBall jball;
+    JBall *jball_ptr{ &jball };
+    jball_ptr->setX(10);
+    jball_ptr->setY(20);
+
+    printf("jball x: %d, ", jball_ptr->getX());
+    printf("jball y: %d\n", jball_ptr->getY());
 
     return 0;
 }
--- Junior/CrashCourse/README.md
+++ Junior/CrashCourse/README.md
@@ -1,4 +1,4 @@
-current page		 - 127
+current page		 - 130
 ______________________________
 installation gdb	 - 82
 installation GDB and GCC - 66
